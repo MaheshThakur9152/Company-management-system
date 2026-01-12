69d215531fda10d1b36ff828b841a7c95f2bee28
Mahesh Thakur <maheshthakur9152@gmail.com>
2026-01-12 22:30:05 +0530

    chore(backend): remove unused upload tests

--- backend/tests/uploads.test.js
+++
@@ -1,6 +0,0 @@
-// TEST REMOVED
-// Per request on 2026-01-03 the tests previously in this file were removed.
-// If you need to restore them later, get them from version control (git).
-
-// Reason: owner requested deletion of this test file.
-
