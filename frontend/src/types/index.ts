a7ea012ae3b1067f8212eff703feb225b15617fc
Amol Yadav <amyssnipet@yahoo.com>
2026-02-27 13:50:02 +0530

    added: new joining option

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -46,7 +46,7 @@ export interface Employee {
   siteId: string;
   photoUrl: string;
   weeklyOff: string;
-  status: 'Active' | 'Inactive' | 'Stopped' | 'Pending' | 'Deleted' | 'On Leave';
+  status: 'Active' | 'Inactive' | 'Stopped' | 'Pending' | 'Deleted' | 'On Leave' | 'New Joining';
   stoppedDate?: string;
   leavingDate?: string;
   returnDate?: string;
